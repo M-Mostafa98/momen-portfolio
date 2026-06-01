efd66e92371e0b374c93016371205f8e7e278102
M-Mostafa98 <me@momen-most.com>
2026-06-01 23:57:28 +0200

    Lightbox: Body-Scroll-Lock + touch-action none, damit Seite beim Swipen nicht scrollt

--- js/app.js
+++ js/app.js
@@ -486,6 +486,34 @@ function Lightbox(_ref) {
       }
     });
   }, [cur]);
+  // Lock background scroll while lightbox is open
+  useEffect(function () {
+    var scrollY = window.scrollY;
+    var body = document.body;
+    var prev = {
+      position: body.style.position,
+      top: body.style.top,
+      left: body.style.left,
+      right: body.style.right,
+      width: body.style.width,
+      overflow: body.style.overflow
+    };
+    body.style.position = "fixed";
+    body.style.top = "-" + scrollY + "px";
+    body.style.left = "0";
+    body.style.right = "0";
+    body.style.width = "100%";
+    body.style.overflow = "hidden";
+    return function () {
+      body.style.position = prev.position;
+      body.style.top = prev.top;
+      body.style.left = prev.left;
+      body.style.right = prev.right;
+      body.style.width = prev.width;
+      body.style.overflow = prev.overflow;
+      window.scrollTo(0, scrollY);
+    };
+  }, []);
   var it = items[cur];
   return /*#__PURE__*/React.createElement("div", {
     onClick: onClose,
@@ -501,7 +529,9 @@ function Lightbox(_ref) {
       alignItems: "center",
       justifyContent: "center",
       animation: "fadeIn 0.25s ease",
-      cursor: "zoom-out"
+      cursor: "zoom-out",
+      touchAction: "none",
+      overscrollBehavior: "contain"
     }
   }, /*#__PURE__*/React.createElement("button", {
     onClick: onClose,
